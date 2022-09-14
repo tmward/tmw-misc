4cbef703c1dadb3206f13be9b8cbc8b8129fc124
Thomas M Ward <thomas@thomasward.com>
2022-09-14 15:19:41 -0400

    Fix 3+ surgeon input and massive refactor

--- mgh_or_schedule.py
+++ mgh_or_schedule.py
@@ -6,7 +6,6 @@ formated table, which it saves to the copy/paste buffer to use in a spreadsheet
 software.
 """
 import csv
-from pprint import pprint
 import re
 import pyperclip
 
@@ -19,12 +18,12 @@ def tidy_patient_class(s):
         "Day Surgery": "Day",
     }
     if s in class_to_abbrev:
-        return class_to_abbrev["s"]
+        return class_to_abbrev[s]
     return s
 
 
 def tidy_patient_name(s):
-    """Remove 'preferred names' in quotes from names."""
+    """Remove "preferred names" from names."""
     return re.sub(r"\".+?\" ", "", s)
 
 
@@ -39,51 +38,54 @@ def tidy_room(s):
 
 
 def tidy_surgeons(s):
-    """Only keep surgeon last name. When 2 surgeons, separate with /."""
-    # Matches last name of either 1 or 2 surgeons
-    # Will need to fix if 3+ surgeons encountered
+    """Only keep surgeon last name. When 2+ surgeons, separate with /."""
     # surgeon names are "Last Names, First M; Last Names2, First2 M2"
-    surgeon_regex = re.compile(r"(^.+?),.*?(; (.+?),.+)?$")
-    surgeon_match = surgeon_regex.fullmatch(s).groups()
-    if surgeon_match[2]:
-        return surgeon_match[0] + "/" + surgeon_match[2]
-    return surgeon_match[0]
+    # so split by "; ", and then only take the last names bit of each name
+    # by splitting by ","
+    return "/".join([surg.split(",")[0] for surg in s.split("; ")])
 
 
-def main():
-    """Take OR table clipboard contents, format nicely, & put into clipboard buffer."""
+def fix_ms_data(dat):
+    """
+    Takes a list of rows with multi-surgeon data and
+    returns a list with one case per row.
+    """
+    # rows are ["OR...", "Surg2...", "Surg3...", "OR...", ...]
+    # so join them together with "; ",
+    # sub in a \n at each new case (representated by "OR" (or "MGW OR"))
+    # then split the resultant string by the new \n to get one case per item
+    return re.sub(r"; ((MGW )?OR \d+)", r"\n\1", "; ".join(dat)).split("\n")
 
-    dat = pyperclip.paste().split("\r\n")[2:]
 
-    final_dat = []
+def extract_cases(dat):
+    """ Takes a TSV copy data and returns a rows of cases."""
+    # ss = single surgeon; ms = multi-surgeon
+    ss_dat = []
     ms_dat = []
     # useful to see if rows aren't full length due to mult surgeons
     ncols = 0
     for n, row in enumerate(dat):
         # add 1st row which is the headers
         if n == 0:
-            final_dat.append(row)
+            ss_dat.append(row)
+            # 1st row always has correct length for a ss case, so set ncols off it
             ncols = len(row.split("\t"))
         # skip blank rows
         elif not row.strip():
             continue
-        # column with correct number of items
+        # single surgeon rows always have correct number of items
         elif len(row.split("\t")) == ncols:
-            final_dat.append(row)
+            ss_dat.append(row)
         # add not fitting rows to the "multi-surgeon" list
         # as when there are multi-surgeons, it copies and pastes them across
         # multiple lines
         else:
             ms_dat.append(row)
+    return ss_dat + fix_ms_data(ms_dat)
 
-    # little janky, only works with 2 surgeons so far
-    # merges the two surgeon two rows into one row
-    n = 0
-    while n < len(ms_dat):
-        final_dat.append(ms_dat[n] + "; " + ms_dat[n + 1])
-        n = n + 2
 
-    # tidy
+def tidy(cases):
+    """Takes list of TSV cases and returns tidied list of case dicts."""
     tidiers = {
         "Patient Class": tidy_patient_class,
         "Patient Name": tidy_patient_name,
@@ -93,18 +95,23 @@ def main():
         "Surgeons": tidy_surgeons,
         "Time": str,
     }
-    reader = csv.DictReader(final_dat, dialect="excel-tab")
-    final_tidied_dat = []
-    for row in reader:
+
+    tidied_cases = []
+    for row in csv.DictReader(cases, dialect="excel-tab"):
+        # Add on cases have no "OR NN" so make it "Add On"
         if row["Progress Status"] == "Add On":
             row["Room"] = "Add On"
-        new_row = {}
+        tidied_case = {}
         for k, v in row.items():
             if k in tidiers:
-                new_row[k] = tidiers[k](v)
-        final_tidied_dat.append(new_row)
+                tidied_case[k] = tidiers[k](v)
+        tidied_cases.append(tidied_case)
 
-    # copy pastable
+    return tidied_cases
+
+
+def make_pastable_tsv(case_dicts):
+    """Takes case_dicts and outputs a sorted TSV to copy to a spreadsheet."""
     columns = [
         "Room",
         "Time",
@@ -114,10 +121,22 @@ def main():
         "Patient Class",
         "Residents/Fellows",
     ]
-    final_data = ["\t".join(columns)]
-    for r in sorted(final_tidied_dat, key=lambda r: r["Room"] + r["Time"]):
-        final_data.append("\t".join(r[c] for c in columns))
-    pyperclip.copy("\n".join(final_data))
+    # add header
+    tsv = ["\t".join(columns)]
+    # add case rows
+    for r in sorted(case_dicts, key=lambda r: r["Room"] + r["Time"]):
+        tsv.append("\t".join(r[c] for c in columns))
+    return "\r\n".join(tsv)
+
+
+def main():
+    """Take OR table clipboard contents, format nicely, & put into clipboard buffer."""
+    # take pasted input, with new rows represented with linebreaks, split it,
+    # and throw out first two rows as they are nonsense always
+    # ss_dat, ms_dat = extract_data(pyperclip.paste().split("\r\n")[2:])
+    cases = extract_cases(pyperclip.paste().splitlines()[2:])
+    tidied_case_dicts = tidy(cases)
+    pyperclip.copy(make_pastable_tsv(tidied_case_dicts))
 
 
 if __name__ == "__main__":
